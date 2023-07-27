9c5b7e37ad5098822da1314e9c0cb094f64fe2ec
Nwodo Thomas <Nwodothomas@gmail.com>
2023-07-27 15:20:16 +0100

    updated 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,30 +1,65 @@
 #!/usr/bin/env python3
-""" LIFOCache module
+"""Create a class LIFOCache that inherits from
+BaseCaching and is a caching system:
+
+You must use self.cache_data - dictionary from
+the parent class BaseCaching
+You can overload def __init__(self): but don’t
+forget to call the parent init: super().__init__()
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do
+anything.
+If the number of items in self.cache_data is higher
+that BaseCaching.MAX_ITEMS:
+you must discard the last item put in cache
+(LIFO algorithm)
+you must print DISCARD: with the key discarded and
+following by a new line
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
 """
-from base_caching import BaseCaching
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class LIFOCache(BaseCaching):
-    """ LIFOCache defines a caching system using LIFO eviction policy
+    """_summary_
     """
+
     def __init__(self):
-        """ Initialize LIFOCache
+        """_summary_
         """
         super().__init__()
 
     def put(self, key, item):
-        """ Add an item in the cache using LIFO eviction policy
+        """_summary_
+
+        Args:
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                # Get the last key (LIFO) to discard and remove it from the cache
-                last_key = next(reversed(self.cache_data))
-                del self.cache_data[last_key]
-                print("DISCARD:", last_key)
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                # delete the last item in the dictionary
+                last_key, last_value = self.cache_data.popitem()
+                print("DISCARD: {}". format(last_key))
+
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key from the cache using LIFO eviction policy
+        """return the value in self.cache_data linked to key
+
+        Args:
+                        key (_type_): _description_
         """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
