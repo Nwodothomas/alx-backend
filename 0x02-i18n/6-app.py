21cd7a5b45e50f73257f0bd85fd9ca5d065c0f3c
Nwodo Thomas <Nwodothomas@gmail.com>
2023-08-02 14:52:38 +0100

    added 6-app.py & 6-index.html

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
-"""A simple Flask app with user login system and Babel for internationalization
+"""A simple flask app
 """
 
+
 from flask import Flask, render_template, request, g
-from flask_babel import Babel, gettext
+from flask_babel import Babel
+
 
 class Config(object):
-    """Configuration class for Flask-Babel
+    """_summary_
 
-    Attributes:
-        LANGUAGES (list): List of available languages for translation.
-        BABEL_DEFAULT_LOCALE (str): Default locale for Babel.
-        BABEL_DEFAULT_TIMEZONE (str): Default timezone for Babel.
+    Returns:
+                    _type_: _description_
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-# Mock user database
+
+# configure the flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -25,58 +32,59 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-# Configure the Flask app
-app = Flask(__name__)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
-babel = Babel(app)
+
+def get_user():
+    """returns a user dictionary or None if the ID cannot be found
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """_summary_
+    """
+    user = get_user()
+    g.user = user
+
 
 @babel.localeselector
 def get_locale():
-    """Get the locale based on user preferences, request parameters, or accept_languages.
+    """_summary_
 
     Returns:
-        str: The best-matching locale from the supported languages.
+                    _type_: _description_
     """
-    # Check if the 'locale' parameter is present in the request args
-    requested_locale = request.args.get('locale')
+    # Locale from URL parameters
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
 
-    # Check if a user is logged in and has a preferred locale
-    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
-        return g.user['locale']
+    # Locale from user settings
+    if g.user:
+        locale = g.user.get('locale')
+        if locale and locale in app.config['LANGUAGES']:
+            return locale
 
-    # If 'locale' parameter is present and is a supported locale, return it
-    if requested_locale and requested_locale in app.config['LANGUAGES']:
-        return requested_locale
+    # ocale from request header
+    locale = request.headers.get('locale', None)
+    if locale in app.config['LANGUAGES']:
+        return locale
 
-    # Resort to the default behavior - get locale from request.accept_languages
+        # Default locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-def get_user(user_id):
-    """Get the user dictionary based on the user ID.
+# babel.init_app(app, locale_selector=get_locale)
 
-    Args:
-        user_id (int): ID of the user to find.
-
-    Returns:
-        dict or None: The user dictionary if found, None if not found.
-    """
-    return users.get(user_id)
-
-@app.before_request
-def before_request():
-    """Function executed before all other functions to set the user in flask.g.user."""
-    user_id = request.args.get('login_as', type=int)
-    g.user = get_user(user_id)
 
 @app.route('/')
 def index():
-    """Render the 6-index.html template.
-
-    Returns:
-        str: Rendered content of the 6-index.html template.
+    """_summary_
     """
-    return render_template('6-index.html')
+    return render_template('5-index.html')
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
