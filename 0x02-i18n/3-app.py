2aa41e9ff34e2bfd8dc59ecfa5e5712b94253c3b
Nwodo Thomas <Nwodothomas@gmail.com>
2023-08-02 13:21:58 +0100

    updated 3-app.py file

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,42 +2,42 @@
 """A simple Flask app with Babel for internationalization
 """
 
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-class Config(object):
-    """Configuration class for Flask-Babel
 
-    Attributes:
-        LANGUAGES (list): List of available languages for translation.
-        BABEL_DEFAULT_LOCALE (str): Default locale for Babel.
-        BABEL_DEFAULT_TIMEZONE (str): Default timezone for Babel.
+class Config(object):
+    """_summary_
+
+    Returns:
+            _type_: _description_
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-# Configure the Flask app
+
+# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
-    """Get the locale based on request.accept_languages.
+    """_summary_
 
     Returns:
-        str: The best-matching locale from the supported languages.
+            _type_: _description_
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
-    """Render the 3-index.html template.
-
-    Returns:
-        str: Rendered content of the 3-index.html template.
+    """_summary_
     """
     return render_template('3-index.html')
 
