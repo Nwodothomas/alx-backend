c73c4170dac311cc4ccd0f11a34ff199147f81a9
Nwodo Thomas <Nwodothomas@gmail.com>
2023-08-02 13:50:17 +0100

    updated 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,54 +1,53 @@
 #!/usr/bin/env python3
-"""A simple Flask app
+"""A simple flask app
 """
+
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-class Config(object):
-    """Configuration class for Flask-Babel
 
-    Attributes:
-        LANGUAGES (list): List of available languages for translation.
-        BABEL_DEFAULT_LOCALE (str): Default locale for Babel.
-        BABEL_DEFAULT_TIMEZONE (str): Default timezone for Babel.
+class Config(object):
+    """_summary_
+
+    Returns:
+            _type_: _description_
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-# Configure the Flask app
+
+# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
-    """Get the locale based on request parameters or accept_languages.
+    """_summary_
 
     Returns:
-        str: The best-matching locale from the supported languages.
+            _type_: _description_
     """
-    # Check if the 'locale' parameter is present in the request args
-    requested_locale = request.args.get('locale')
-    
-    # If 'locale' parameter is present and is a supported locale, return it
-    if requested_locale and requested_locale in app.config['LANGUAGES']:
-        return requested_locale
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
 
-    # If 'locale' parameter is not present or is not a supported locale,
-    # resort to the previous default behavior
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+# babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route('/')
 def index():
-    """Render the 4-index.html template.
-
-    Returns:
-        str: Rendered content of the 4-index.html template.
+    """_summary_
     """
     return render_template('4-index.html')
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
